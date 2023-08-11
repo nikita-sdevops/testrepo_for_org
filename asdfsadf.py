6691b1eb21dda5f4817f98c7d65462f7e3522658
Nikita <nikitanec@gmail.com>
2023-08-11 19:47:19 +0300

    updated asdfsadf.py

--- asdfsadf.py
+++ asdfsadf.py
@@ -1,1 +1,9 @@
-print('hello world')
+def do_something(a,b,c,d): return c*a-b+d if (a > 10 and b < c or d == 0) else (a+b-c)*d
+
+a = [1, 2, '3', 4, 5]
+result = 0
+for i in range(len(a)):
+  if type(a[i]) is str:
+    a[i] = int(a[i])
+  result += a[i]
+print("Result is " + str(result + do_something(12, 4, 5, 6) * 3 - 4))
